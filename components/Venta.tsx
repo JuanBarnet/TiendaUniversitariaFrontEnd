72a1615425b99f660c333b6a3efc7aabf8539862
Nicolás Cofré <[nicolas.cofre@alumnos.ucn.cl]>
2021-11-18 20:16:54 -0300

    fix: arreglar la vista de las ventas

--- components/Venta.tsx
+++ components/Venta.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Swal from 'sweetalert2';
 import { useRouter } from 'next/router';
+import { ImCancelCircle } from "react-icons/im";
 
 import { registrarVenta } from '../api/ventas';
 
@@ -18,6 +19,42 @@ export const Venta = (): JSX.Element =>{
     //para re-renderizar los productos crear su nuevo componente que es cada fila
     //const [productoCantidad,setProductoCantidad] = useState({});
 
+    function useWindowDimensions() {
+        const [width, setWidth] = React.useState(window.innerWidth);
+        const [height, setHeight] = React.useState(window.innerHeight);
+      
+        const updateWidthAndHeight = () => {
+          setWidth(window.innerWidth);
+          setHeight(window.innerHeight);
+        };
+      
+        React.useEffect(() => {
+          window.addEventListener("resize", updateWidthAndHeight);
+          return () => window.removeEventListener("resize", updateWidthAndHeight);
+        });
+      
+        return {
+          width,
+          height,
+        }
+    }
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const handleScroll = () => {
+        const position = window.pageXOffset;
+        setScrollPosition(position);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const { width, height } = useWindowDimensions()
+    const limite = 900;
     
     function getProductosCarro(){
 
@@ -109,7 +146,7 @@ export const Venta = (): JSX.Element =>{
         Swal.fire({
             icon: 'success',
             title: 'Venta Exitosa',
-            text: 'La venta se ha realizadp con exito.'
+            text: 'La venta se ha realizado con éxito.'
         }).then(() => {
             Router.push("/Tienda/Perfil");
         });
@@ -138,26 +175,30 @@ export const Venta = (): JSX.Element =>{
 
     return (
         <>
-            <div className="container d-flex">
-                <div className="container-fluid" id="contenedor_lista" style={{height:'50px', marginTop:'50px', width:'1080px'}}>
+            <div className="container-fluid" id="contenedor" style={{width:width, height: height - 100, marginLeft:'0'}}>
+                <div className="col" style={{paddingLeft:'0', paddingRight:'0', paddingTop:'50px'}}>
+                
+                <div className="container-fluid" id="contenedor_lista" style={{height:'50px', marginTop:'50px', width:'1110px', display:'flex', alignItems:'center', justifyContent:'center'}}>
                     <div className="row" style={{width:'1080px', height:'50px', display:'flex', marginLeft:'15px', marginRight:'15px', backgroundColor:'#425563', borderTopLeftRadius:'15px', borderTopRightRadius:'15px'}}>
                         <div id="header_lista" style={{width:'180px', height:'50px'}}>
                             <h2 id="text_lista">
                                 Nombre
                             </h2>
                         </div>
-                        <div id="header_lista" style={{width:'180px'}}>
-                            
-                            <h2 id="text_lista">
-                                Cantidad Producto
-                            </h2>
-                        </div>
+
                         <div id="header_lista" style={{width:'180px'}}>
                             
                             <h2 id="text_lista">
                                 Stock Actual
                             </h2>
 
+                        </div>
+
+                        <div id="header_lista" style={{width:'180px'}}>
+                            
+                            <h2 id="text_lista">
+                                Cantidad Producto
+                            </h2>
                         </div>
 
                         <div id="header_lista" style={{width:'180px'}}>
@@ -183,61 +224,75 @@ export const Venta = (): JSX.Element =>{
                             </h2>
                             
                         </div>
+                    
+                        </div>
+                    </div>
 
+                     {/*<div className="row" style={{width:'1080px', height:'50px', display:'flex', marginLeft:'15px', marginRight:'15px', backgroundColor:'#425563', borderTopLeftRadius:'15px', borderTopRightRadius:'15px'}}>*/}
                         {
                             productos.map((producto)=>{
                                 return(
-                                    <div className="row" key={producto.codigo_interno} id="fila_lista">
-                                        <div style={{width:'180px'}}>
-                                            <h2 id="text_lista" style={{width:'180px', height:'100px', paddingLeft:'20px', display:'flex', alignItems:'center'}}>
-                                                {producto.nombre}
-                                            </h2>
-                                        </div>
-                                        <div style={{width:'180px'}}>
-                                            <h2 id="text_lista" style={{width:'180px', height:'100px', paddingLeft:'80px', display:'flex', alignItems:'center'}}>
-                                                {producto.cantidad}
-                                            </h2>
-                                        </div>
-                                        <div style={{width:'180px'}}>
-                                            <h2 id="text_lista" style={{width:'180px', height:'100px', paddingLeft:'80px', display:'flex', alignItems:'center'}}>
-                                                {producto.cantidad_actual}
-                                            </h2>
-                                        </div>
-                                        <div id="elementos_lista" style={{width:'180px'}}>
-                                            <button type="submit" value={productos.indexOf(producto)} onClick={(e) => {disminuirProducto(parseInt((e.target as HTMLButtonElement).value));}} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'130px', display:'flex', alignItems:'center', justifyContent:'center'}}>                      
-                                                -      
-                                            </button>
-                                        </div>
-                                        <div id="elementos_lista" style={{width:'178px'}}>
-                                            <button type="submit" value={productos.indexOf(producto)} onClick={(e) => {aumentarProducto(parseInt((e.target as HTMLButtonElement).value));}} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'130px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
-                                                +
-                                            </button>
-                                        </div>
-                                        <div id="elementos_lista" style={{width:'178px'}}>
-                                            <button type="submit" value={productos.indexOf(producto)} onClick={(e) => {eliminarProducto(parseInt((e.target as HTMLButtonElement).value),producto.codigo_interno);}} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'130px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
-                                                X
-                                            </button>
+                                    <div className="container-fluid" id="contenedor_lista" style={{height:'100px', width:'1110px', display:'flex', alignItems:'center', justifyContent:'center'}}>
+                                        <div className="row" key={producto.codigo_interno} id="fila_lista" style={{height:'100px', width:'1080px', marginLeft:'15px', marginRight:'15px'}}>
+                                            <div style={{width:'180px'}}>
+                                                <h2 id="text_lista" style={{width:'180px', height:'100px', paddingLeft:'20px', display:'flex', alignItems:'center'}}>
+                                                    {producto.nombre}
+                                                </h2>
+                                            </div>
+                                            <div style={{width:'180px'}}>
+                                                <h2 id="text_lista" style={{width:'180px', height:'100px', paddingLeft:'80px', display:'flex', alignItems:'center'}}>
+                                                    {producto.cantidad_actual}
+                                                </h2>
+                                            </div>
+                                            <div style={{width:'180px'}}>
+                                                <h2 id="text_lista" style={{width:'180px', height:'100px', paddingLeft:'80px', display:'flex', alignItems:'center'}}>
+                                                    {producto.cantidad}
+                                                </h2>
+                                            </div>
+                                            <div id="elementos_lista" style={{width:'180px'}}>
+                                                <button type="submit" value={productos.indexOf(producto)} onClick={(e) => {disminuirProducto(parseInt((e.target as HTMLButtonElement).value));}} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'130px', display:'flex', alignItems:'center', justifyContent:'center'}}>                      
+                                                    -      
+                                                </button>
+                                            </div>
+                                            <div id="elementos_lista" style={{width:'178px'}}>
+                                                <button type="submit" value={productos.indexOf(producto)} onClick={(e) => {aumentarProducto(parseInt((e.target as HTMLButtonElement).value));}} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'130px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
+                                                    +
+                                                </button>
+                                            </div>
+                                            <div id="elementos_lista" style={{width:'178px'}}>
+                                                <button type="submit" value={productos.indexOf(producto)} onClick={(e) => {eliminarProducto(parseInt((e.target as HTMLButtonElement).value),producto.codigo_interno);}} id="boton_cancelar" className="btn btn-primary btn-block btn-lg" style={{height:'40px', width:'130px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
+                                                    <ImCancelCircle style={{pointerEvents:'none'}}/>
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 )
                             })
                         }
 
-                        <div className="p-5 col-8">
-                            <button type="submit" onClick={() => Router.push("/Tienda/ListaProductos")} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'260px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
-                                A&ntilde;adir m&aacute;s productos
-                            </button>
-                        </div>
-
-                        <div className="p-5 col-4">
-                            <button type="submit" onClick={() => realizarVenta()} className="btn btn-primary btn-block btn-lg" id="boton_venta" style={{height:'40px', width:'180px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
-                                Realizar Venta
-                            </button>
-                        </div>
-
- 
+                    <div style={{height:'50px'}}>
 
                     </div>
+                    
+                    <div className="container-fluid" id="contenedor_lista" style={{height: width > limite? '50px': '100px', width:'100%', maxWidth:'1470px', marginLeft:'0', marginRight:'0'}}>
+                        <div className={width > limite? "row":"col"} style={{height: width > limite? '50px': '100px', width:((width * 0.7)), position:'relative', marginLeft: (width*0.15) + scrollPosition - (55/2), marginRight: (width*0.15) + 55}}>
+                            <div style={width > limite? {width:'300px', height:'50px', display:'flex', alignItems:'center', justifyContent:'center'}:{width:'100%', height:'50px', display:'flex', alignItems:'center', justifyContent:'center'}}>
+
+                                <button type="submit" onClick={() => Router.push("/Tienda/ListaProductos")} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'260px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
+                                    <h2 id="text_lista" style={{color:'white', fontWeight:'bold'}}>
+                                        Añadir más productos
+                                    </h2>
+                                </button>
+                            </div>
+                                <div style={width > limite? {width:'300px', height:'50px', display:'flex', alignItems:'center', position:'absolute', right:'0', justifyContent:'center'}:{width:'100%', height:'50px', display:'flex', alignItems:'center', justifyContent:'center'}}>
+                                    <button type="submit" onClick={() => realizarVenta()} className="btn btn-primary btn-block btn-lg" id="boton_lista" style={{height:'40px', width:'260px', display:'flex', alignItems:'center', justifyContent:'center'}}>     
+                                        <h2 id="text_lista" style={{color:'white', fontWeight:'bold'}}>
+                                         Realizar Venta
+                                        </h2>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
 
                 </div>
 
